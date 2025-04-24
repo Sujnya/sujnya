80c39acefd15e4111cfec336d532b89708e6d5e1
Sujnya <sujnyashetti@gmail.com>
2025-04-24 12:50:45 +0530

    save

---
+++ Demo.java
@@ -0,0 +1,11 @@
+package java2;
+
+public class Demo {
+    public static void main(String[] args){
+        int a=10;
+        int b=5;
+        System.out.println("addition: "+(a+b));
+        System.out.println("subtraction: "+(a-b));
+
+    }
+}

621f46b6189cfcd957397dbaeebe8a6be3ea93ec
Sujnya <sujnyashetti@gmail.com>
2025-04-24 12:54:15 +0530

    save

--- Demo.java
+++ Demo.java
@@ -6,6 +6,8 @@ public class Demo {
         int b=5;
         System.out.println("addition: "+(a+b));
         System.out.println("subtraction: "+(a-b));
+        System.out.println("multiplication: "+(a*b));
+        System.out.println("division: "+(a/b));
 
     }
 }
